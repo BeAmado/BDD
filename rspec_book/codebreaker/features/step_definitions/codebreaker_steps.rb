357dfe0fc4bc837044c42c35372b5cbc7326f8ea
Bernardo <bernardoags@gmail.com>
2019-10-07 17:37:27 -0300

    Finished up to the chapter 5. The follow on is the 6th chapter...

--- rspec_book/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ rspec_book/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,3 +1,27 @@
-Given("I am not yet playing") do
+class Output
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
 end
 
+class Singleton
+  def self.output
+    @@instance ||= Output.new
+  end
+end
+
+Given 'I am not yet playing' do
+end
+
+When 'I start a new game' do
+  game = Codebreaker::Game.new(Singleton.output)
+  game.start
+end
+
+Then 'I should see {string}' do |message|
+  expect(Singleton.output.messages).to include(message)
+end
