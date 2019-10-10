1e7b50c454cbd55998ab9852fc999f7de2d53976
Bernardo <bernardoags@gmail.com>
2019-10-10 17:18:16 -0300

    Adjusted the mark for duplicate numbers in the guess and secret

--- rspec_book/codebreaker/lib/codebreaker/marker.rb
+++ rspec_book/codebreaker/lib/codebreaker/marker.rb
@@ -6,22 +6,37 @@ module Codebreaker
 
     def exact_match_count
       (0..3).inject(0) do |count, index|
-        count + (exact_match?(@guess, index) ? 1 : 0)
+        count + (exact_match?(index) ? 1 : 0)
       end
     end
 
     def number_match_count
+=begin
       (0..3).inject(0) do |count, index|
-        count + (number_match?(@guess, index) ? 1 : 0)
+        count + (number_match?(index) ? 1 : 0)
       end
+=end
+      total_match_count - exact_match_count
     end
 
-    def exact_match?(guess, index)
-      @secret[index] == guess[index]
+    def total_match_count
+      count = 0
+      secret = @secret.split('')
+      @guess.split('').map do |n|
+        if secret.include?(n)
+          secret.delete_at(secret.index(n))
+          count += 1
+        end
+      end
+      count
     end
 
-    def number_match?(guess, index)
-      !exact_match?(guess, index) && @secret.include?(guess[index])
+    def exact_match?(index)
+      @secret[index] == @guess[index]
+    end
+
+    def number_match?(index)
+      !exact_match?(index) && @secret.include?(@guess[index])
     end
   end
 end
