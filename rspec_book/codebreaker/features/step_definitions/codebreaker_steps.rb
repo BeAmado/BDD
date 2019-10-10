860c3bfa61bba8fb7ba75ceb3f33080ffd9f3707
Bernardo <bernardoags@gmail.com>
2019-10-10 14:45:23 -0300

    Created a Mark class

--- rspec_book/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ rspec_book/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -14,14 +14,31 @@ class Singleton
   end
 end
 
+########### Scenario start game ##########################
 Given 'I am not yet playing' do
 end
 
 When 'I start a new game' do
   game = Codebreaker::Game.new(Singleton.output)
-  game.start
+  game.start('1234')
 end
 
 Then 'I should see {string}' do |message|
   expect(Singleton.output.messages).to include(message)
 end
+##########################################################
+
+######### Scenarios submit guess #########################
+Given 'the secret code is {string}' do |secret|
+  @game = Codebreaker::Game.new(Singleton.output)
+  @game.start(secret)
+end
+
+When 'I guess {string}' do |guess|
+  @game.guess(guess)
+end
+
+Then 'the mark should be {string}' do |mark|
+  expect(Singleton.output.messages).to include(mark)
+end
+#########################################################
